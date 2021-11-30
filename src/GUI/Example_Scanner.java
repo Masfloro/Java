e3dc7ef017add715d55938ecb0bfc9cfa3546299
Carlos Marcel <marcel241201@hotmail.com>
2021-11-30 13:08:39 -0500

    Creacion de una nueva clase

--- src/GUI/Example_Scanner.java
+++ src/GUI/Example_Scanner.java
@@ -2,17 +2,19 @@ package GUI;
 import java.util.Scanner;
 public class Example_Scanner {
 
-    public void mandarMensaje(){
+    public static void mandarMensaje(){
         //Aqui se mandara el mensaje
         System.out.println("Hola mundo");
     }
     
     public static void main(String[] args) {
-
+        mandarMensaje();
         Scanner objScanner = new Scanner(System.in);
 
         int edad;
         String nombre;
+
+        
 
         System.out.println("Ingrese su nombre");
         nombre = objScanner.nextLine();
